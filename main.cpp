1d18390b43f2b706f2dab658c08eef3b1ee62b0b
Sedo-KFM <fedor122448@gmail.com>
2024-01-04 18:01:21 +0400

    Add reversed z_function

--- main.cpp
+++ main.cpp
@@ -150,7 +150,7 @@ namespace my {
         return res;
     }
 
-    vector<int> z_function(string s) {
+    vector<int> straight_z_function(string s) {
         int n = (int) s.length();
         vector<int> z(n);
         for (int i = 1, l = 0, r = 0; i < n; ++i) {
@@ -162,7 +162,20 @@ namespace my {
                 l = i, r = i + z[i] - 1;
         }
         return z;
+    }
 
+    vector<int> reversed_z_function(string s) {
+        int n = (int) s.length();
+        vector<int> z(n);
+        for (int i = n - 2, l = n - 1, r = n - 1; i >= 0; --i) {
+            if (i >= l)
+                z[i] = min(i - l + 1, z[n - (r - i) - 1]);
+            while (i - z[i] >= 0 && s[n - 1 - z[i]] == s[i - z[i]])
+                ++z[i];
+            if (i - z[i] + 1 < l)
+                r = i, l = i - z[i] + 1;
+        }
+        return z;
     }
 }
 
